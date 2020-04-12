1024e5237b64f11baa27bc1f5477559f73399c3b
KosukeShimizu <thimi0412@icloud.com>
2020-04-12 15:38:32 +0900

    投票項目を編集できる設定を追加

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
@@ -8,6 +13,8 @@ class QuestionAdmin(admin.ModelAdmin):
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
