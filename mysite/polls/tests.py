943e0c238522e61cc1d8e4593ab1831cacd2ded1
KosukeShimizu <thimi0412@icloud.com>
2020-04-12 14:36:06 +0900

    DetailViewのテストを作成

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -79,3 +79,19 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(
+            question_text='Future question.', days=5)
+        url = reverse('polls:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_post_question(self):
+        past_question = create_question(
+            question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
